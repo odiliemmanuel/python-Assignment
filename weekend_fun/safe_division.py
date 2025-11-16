a8cdac1c8e0a75126d28f98c4906cbb201c36493
Odili <odiliejeh09@gmail.com>
2025-11-16 02:53:44 +0100

    safe division

---
+++ weekend_fun/safe_division.py
@@ -0,0 +1,11 @@
+first_integer = int(input("Enter value for first Integer"))
+
+second_integer = int(input("Enter value for second Integer"))
+
+division = first_integer / second_integer 
+
+if (second_integer != 0):
+    print(division)
+
+if (second_integer == 0):
+    print("Cannot divide by zero")
