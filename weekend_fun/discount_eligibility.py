f9279a03302985f6b0a6c4ba7a0485d55eb02e87
Odili <odiliejeh09@gmail.com>
2025-11-16 06:48:48 +0100

    assignment update

--- weekend_fun/discount_eligibility.py
+++ weekend_fun/discount_eligibility.py
@@ -1,1 +1,14 @@
+total_bill = float(input("How much is your your bill "))
 
+is_member = str(input("Are you a member "))
+
+if(total_bill >= 1000 and is_member == "yes"):
+    print("10% off")
+
+elif(total_bill >= 1000 and is_member != "yes"):
+    print("5% off")
+
+else:
+    print("No discount")
+    
+    
